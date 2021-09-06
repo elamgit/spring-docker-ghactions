785c34016151a0032dc6fb96cae4bd22d733c67e
DESKTOP-T5FDJL3\elama <elamaran@gmail.com>
2021-09-05 22:09:36 -0400

    Initial commit

---
+++ src/main/java/com/example/springdockerghactions/SpringDockerGhactionsApplication.java
@@ -0,0 +1,13 @@
+package com.example.springdockerghactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDockerGhactionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDockerGhactionsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springdockerghactions/SpringDockerGhactionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springdockerghactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDockerGhactionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
